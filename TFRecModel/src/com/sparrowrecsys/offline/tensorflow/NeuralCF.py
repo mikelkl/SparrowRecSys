c04d4f6456027d62b37bf7bce9d4eaa65d848475
mikelkl <mikelkl@foxmail.com>
2022-04-04 15:35:46 +0800

    refactor: run through on Windows OS

--- TFRecModel/src/com/sparrowrecsys/offline/tensorflow/NeuralCF.py
+++ TFRecModel/src/com/sparrowrecsys/offline/tensorflow/NeuralCF.py
@@ -1,13 +1,12 @@
 import tensorflow as tf
+import os
 
 # Training samples path, change to your local path
 training_samples_file_path = tf.keras.utils.get_file("trainingSamples.csv",
-                                                     "file:///Users/zhewang/Workspace/SparrowRecSys/src/main"
-                                                     "/resources/webroot/sampledata/trainingSamples.csv")
+                                                     "file:///D:/Projects/SparrowRecSys/src/main/resources/webroot/sampledata/trainingSamples.csv")
 # Test samples path, change to your local path
 test_samples_file_path = tf.keras.utils.get_file("testSamples.csv",
-                                                 "file:///Users/zhewang/Workspace/SparrowRecSys/src/main"
-                                                 "/resources/webroot/sampledata/testSamples.csv")
+                                                 "file:///D:/Projects/SparrowRecSys/src/main/resources/webroot/sampledata/testSamples.csv")
 
 
 # load sample as tf dataset
@@ -96,7 +95,7 @@ for prediction, goodRating in zip(predictions[:12], list(test_dataset)[0][1][:12
 
 tf.keras.models.save_model(
     model,
-    "file:///Users/zhewang/Workspace/SparrowRecSys/src/main/resources/webroot/modeldata/neuralcf/002",
+    "../../../../../../src/main/resources/webroot/modeldata/neuralcf/002",
     overwrite=True,
     include_optimizer=True,
     save_format=None,
--- src/main/java/com/sparrowrecsys/online/recprocess/RecForYouProcess.java
+++ src/main/java/com/sparrowrecsys/online/recprocess/RecForYouProcess.java
@@ -115,9 +115,11 @@ public class RecForYouProcess {
             return;
         }
 
+        //保存所有样本的JSON数组
         JSONArray instances = new JSONArray();
         for (Movie m : candidates){
             JSONObject instance = new JSONObject();
+            //为每个样本添加特征，userId和movieId
             instance.put("userId", user.getUserId());
             instance.put("movieId", m.getMovieId());
             instances.put(instance);
@@ -126,12 +128,15 @@ public class RecForYouProcess {
         JSONObject instancesRoot = new JSONObject();
         instancesRoot.put("instances", instances);
 
+        //请求TensorFlow Serving API
         //need to confirm the tf serving end point
         String predictionScores = asyncSinglePostRequest("http://localhost:8501/v1/models/recmodel:predict", instancesRoot.toString());
         System.out.println("send user" + user.getUserId() + " request to tf serving.");
 
+        //获取返回预估值
         JSONObject predictionsObject = new JSONObject(predictionScores);
         JSONArray scores = predictionsObject.getJSONArray("predictions");
+        //将预估值加入返回的map
         for (int i = 0 ; i < candidates.size(); i++){
             candidateScoreMap.put(candidates.get(i), scores.getJSONArray(i).getDouble(0));
         }
